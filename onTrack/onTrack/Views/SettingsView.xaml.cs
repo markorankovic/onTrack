293471fd120226c16d0192a650b430e872532c2f
Marko Ranković <marko@markorankovic.com>
2022-10-06 15:43:38 +0100

    Blue dot replaces the black dot on a checked radio button

--- onTrack/onTrack/Views/SettingsView.xaml.cs
+++ onTrack/onTrack/Views/SettingsView.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
-using System.Media;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using Microsoft.Toolkit.Uwp.Notifications;
 using onTrack.Reinforcements;
-using Windows.Foundation.Collections;
 
 namespace onTrack.Views
 {
