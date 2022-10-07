400748524a678288866fa738cde94a0c839243cb
Marko Ranković <marko@markorankovic.com>
2022-10-07 12:59:02 +0100

    Formatted Slider moved to newly created Components folder

---
+++ onTrack/onTrack/Components/FormattedSlider.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace onTrack.Components
+{
+    public class FormattedSlider : Slider
+    {
+        private ToolTip _autoToolTip;
+        private string _autoToolTipFormat;
+
+        public string AutoToolTipFormat
+        {
+            get { return _autoToolTipFormat; }
+            set { _autoToolTipFormat = value; }
+        }
+
+        protected override void OnThumbDragStarted(DragStartedEventArgs e)
+        {
+            base.OnThumbDragStarted(e);
+            FormatAutoToolTipContent();
+        }
+
+        protected override void OnThumbDragDelta(DragDeltaEventArgs e)
+        {
+            base.OnThumbDragDelta(e);
+            FormatAutoToolTipContent();
+        }
+
+        private void FormatAutoToolTipContent()
+        {
+            if (!string.IsNullOrEmpty(AutoToolTipFormat))
+            {
+                AutoToolTip.Content = string.Format(AutoToolTipFormat, AutoToolTip.Content);
+            }
+        }
+
+        private ToolTip AutoToolTip
+        {
+            get
+            {
+                if (_autoToolTip == null)
+                {
+                    FieldInfo field = typeof(Slider).GetField(
+                            "_autoToolTip",
+                            BindingFlags.NonPublic |
+                            BindingFlags.Instance);
+                    _autoToolTip = field.GetValue(this) as ToolTip;
+                }
+                return _autoToolTip;
+            }
+        }
+    }
+}
