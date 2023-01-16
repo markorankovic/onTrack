20f9bd4ba07b16a8948784b9a6c2a2c22a83a168
Marko Ranković <marko@markorankovic.com>
2023-01-16 20:52:25 +0000

    The task list nav button is now interactive

--- onTrack/onTrack/MainWindow.xaml.cs
+++ onTrack/onTrack/MainWindow.xaml.cs
@@ -52,21 +52,37 @@ namespace onTrack
             {
                 timerNav.Opacity = 1;
                 settingsNav.Opacity = 0.5;
-            } else
+                taskListNav.Opacity = 0.5;
+            } else if (((MainViewModel)DataContext).SelectedViewModel is SettingsViewModel)
             {
                 timerNav.Opacity = 0.5;
                 settingsNav.Opacity = 1;
+                taskListNav.Opacity = 0.5;
+            } else
+            {
+                timerNav.Opacity = 0.5;
+                settingsNav.Opacity = 0.5;
+                taskListNav.Opacity = 1;
             }
         }
 
         private void TimerNav_Click(object sender, RoutedEventArgs e)
         {
             timerNav.Opacity = 1;
+            taskListNav.Opacity = 0.5;
+            settingsNav.Opacity = 0.5;
+        }
+
+        private void TaskListNav_Click(object sender, RoutedEventArgs e)
+        {
+            taskListNav.Opacity = 1;
+            timerNav.Opacity = 0.5;
             settingsNav.Opacity = 0.5;
         }
 
         private void SettingsNav_Click(object sender, RoutedEventArgs e)
         {
+            taskListNav.Opacity = 0.5;
             timerNav.Opacity = 0.5;
             settingsNav.Opacity = 1;
         }
