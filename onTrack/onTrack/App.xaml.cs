3b00908495815bac73136d160e0dc741c2022f20
Marko Ranković <marko@markorankovic.com>
2023-01-20 22:04:44 +0000

    Able to create the task hierarchy from the TaskTree class

--- onTrack/onTrack/App.xaml.cs
+++ onTrack/onTrack/App.xaml.cs
@@ -1,16 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Media;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
-using System.Windows.Controls;
-using Microsoft.Toolkit.Uwp.Notifications;
-using Windows.Foundation.Collections;
 
 namespace onTrack
 {
-    public partial class App : Application { }
+    public class TaskTree : INotifyPropertyChanged
+    {
+        TaskItem _Root;
+        public TaskItem Root { get { return _Root; } set { _Root = value; NotifyPropertyChanged("Root"); } }
+
+        public List<TaskItem> Items { get; set; }
+
+        public TaskTree(TaskItem root)
+        {
+            _Root = root;
+            Items = new List<TaskItem>();
+            Items.Add(root);
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+    public class TaskItem : INotifyPropertyChanged
+    {
+        TaskItem? Parent;
+        public List<TaskItem> Children { get; set; }
+        string _Task = "";
+        public string Task { get { return _Task; } set { _Task = value; NotifyPropertyChanged("Task"); } }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public TaskItem()
+        {
+            Children = new List<TaskItem>();
+        }
+
+        public void AddChild(TaskItem task)
+        {
+            Children.Add(task);
+            task.Parent = this;
+        }
+
+        public void RemoveChild(TaskItem task)
+        {
+            Children.Remove(task);
+        }
+
+        public void RemoveFromParent()
+        {
+            Parent?.RemoveChild(this);
+        }
+
+        public void SetTask(string task)
+        {
+            Task = task;
+        }
+
+        public void SetParent(TaskItem Parent)
+        {
+            this.Parent = Parent;
+        }
+
+        public string GetTask()
+        {
+            return Task;
+        }
+    }
+
+    public partial class App : Application {
+        public App()
+        {
+            this.InitializeComponent();
+
+            TaskItem parent = new TaskItem();
+            parent.SetTask("Your Objective");
+
+            TaskItem goal1 = new TaskItem();
+            goal1.SetTask("Goal 1");
+
+            TaskItem goal2 = new TaskItem();
+            goal2.SetTask("Goal 2");
+
+            TaskItem goal2_1 = new TaskItem();
+            goal2_1.SetTask("Goal 2-1");
+
+            parent.AddChild(goal1);
+            parent.AddChild(goal2);
+            goal2.AddChild(goal2_1);
+
+            TaskTree taskTree = new TaskTree(parent);
+
+            Resources.Add("taskList", taskTree);
+        }
+    }
 }
--- onTrack/onTrack/Views/TaskListView.xaml.cs
+++ onTrack/onTrack/Views/TaskListView.xaml.cs
@@ -1,28 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace onTrack.Views
 {
-    /// <summary>
-    /// Interaction logic for TaskListView.xaml
-    /// </summary>
     public partial class TaskListView : UserControl
     {
         public TaskListView()
         {
             InitializeComponent();
+            DataContext = ((TaskTree) Application.Current.Resources["taskList"]);
         }
     }
 }
