cf1b1692bab65b150cfb9ccb999509e0bfb2eb69
Marko Ranković <marko@markorankovic.com>
2023-03-16 16:51:40 +0000

    PersonModel can be displayed & edited in the view

---
+++ onTrack/PersistentStorageApp/PersonModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace PersistentStorageApp
+{
+    public class PersonModel
+    {
+        string _name;
+        public string name {
+            get { return _name; }
+            set {
+                _name = value;
+            }
+        }
+
+        ObservableCollection<Address> _addresses;
+        public ObservableCollection<Address> addresses {
+            get { return _addresses; }
+            set {
+                _addresses = value;
+            }
+        }
+
+        public class Address {
+            public string _line;
+            public string line { set { _line = value; } get { return _line; } }
+            public Address(string line)
+            {
+                this._line = line;
+            }
+        }
+
+        public PersonModel(string name, ObservableCollection<Address> addresses)
+        {
+            this._name = name;
+            this._addresses = addresses;
+        }
+    }
+}
---
+++ onTrack/PersistentStorageApp/PersonView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace PersistentStorageApp
+{
+    public partial class PersonView : UserControl
+    {
+        public PersonView()
+        {
+            InitializeComponent();
+
+            var addresses = new ObservableCollection<PersonModel.Address>();
+            addresses.Add(new PersonModel.Address("Maddison Square"));
+            addresses.Add(new PersonModel.Address("Old Trafford"));
+
+            DataContext = new PersonModel("John", addresses);
+        }
+    }
+}
