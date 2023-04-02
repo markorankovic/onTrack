a92357fcc8d3fc0bc0b6041210295f08bce8daf7
Marko Ranković <marko@markorankovic.com>
2023-04-02 20:04:47 +0100

    The complete app state is now persisted in a JSON file

--- onTrack/onTrack/App.xaml.cs
+++ onTrack/onTrack/App.xaml.cs
@@ -1,21 +1,66 @@
-﻿using System;
+﻿using onTrack.Reinforcements;
+using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
 using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Input;
 
 namespace onTrack
 {
+    public class AppStore
+    {
+        [JsonInclude]
+        public TaskTree taskTree;
+        [JsonInclude]
+        public Settings settings;
+    }
+    public class Point2D
+    {
+        [JsonInclude]
+        public int x;
+        [JsonInclude]
+        public int y;
+    }
+    public class Settings
+    {
+        // Timer
+        [JsonInclude]
+        public double Duration;
+        // Reinforcement
+        [JsonInclude]
+        public string reinforcement;
+        // Audio
+        [JsonInclude]
+        public string alarmName;
+        // Automations
+        [JsonInclude]
+        public bool autoPausePlayEnabled;
+        [JsonInclude]
+        public bool autoFocusEnabled;
+        [JsonInclude]
+        public Key? autoPauseKey;
+        [JsonInclude]
+        public Point2D? autoFocusClickLocation;
+        [JsonInclude]
+        public Point2D? autoPlayClickLocation;
+    }
+
     public class TaskTree : INotifyPropertyChanged
     {
         TaskItem _Root;
+        [JsonIgnore]
         public TaskItem Root { get { return _Root; } set { _Root = value; NotifyPropertyChanged("Root"); } }
 
         public ObservableCollection<TaskItem> Items { get; set; }
-
-        TaskItem? _CurrentTask;
+        [JsonInclude]
+        public TaskItem? _CurrentTask;
+        [JsonIgnore]
         public TaskItem? CurrentTask { 
             get { return _CurrentTask; } 
             set {
@@ -30,9 +75,20 @@ namespace onTrack
         public TaskTree(TaskItem root)
         {
             _Root = root;
-            CurrentTask = _Root;
             Items = new ObservableCollection<TaskItem>();
             Items.Add(root);
+        }
+
+        public void SetParentOnChildTasks(TaskItem taskItem)
+        {
+            if (taskItem.Id.Equals(_CurrentTask.Id)) { 
+                CurrentTask = taskItem; 
+            }
+            foreach (TaskItem t in taskItem.Children)
+            {
+                t.SetParent(taskItem);
+                SetParentOnChildTasks(t);
+            }
         }
 
         public void SetCurrentTask(TaskItem task)
@@ -55,6 +111,8 @@ namespace onTrack
     }
     public class TaskItem : INotifyPropertyChanged
     {
+        [JsonInclude]
+        public Guid Id = Guid.NewGuid();
         TaskItem? Parent;
         public ObservableCollection<TaskItem> Children { get; set; }
         string _Task = "";
@@ -70,6 +128,7 @@ namespace onTrack
             }
         }
 
+        [JsonInclude]
         public bool IsDone = false;
 
         public event PropertyChangedEventHandler? PropertyChanged;
@@ -171,16 +230,83 @@ namespace onTrack
     }
 
     public partial class App : Application {
+
         public App()
         {
             InitializeComponent();
+        }
 
-            TaskItem parent = new TaskItem();
-            parent.SetTask("Your Objective");
+        void Save()
+        {
+            TaskTree taskTree = (TaskTree) Current.Resources["taskList"];
+            Settings settings = new Settings();
+            settings.Duration = Timer.Duration;
+            settings.reinforcement = Timer.GetReinforcement().GetType().Name;
+            settings.alarmName = Timer.GetAlarmName();
+            settings.autoPausePlayEnabled = Timer.autoPausePlay;
+            settings.autoFocusEnabled = Timer.autoFocus;
+            settings.autoFocusClickLocation = new Point2D() { x = Timer.autoFocusClickLocation.x, y = Timer.autoFocusClickLocation.y };
+            settings.autoPlayClickLocation = new Point2D() { x = Timer.autoPlayClickLocation.x, y = Timer.autoPlayClickLocation.y };
+            settings.autoPauseKey = Timer.autoPauseKey;
+            AppStore store = new AppStore();
+            store.taskTree = taskTree;
+            store.settings = settings;
 
-            TaskTree taskTree = new TaskTree(parent);
+            string jsonString = JsonSerializer.Serialize(store, new JsonSerializerOptions { WriteIndented = true });
+            string currentPath = Environment.CurrentDirectory;
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(currentPath, "appStore.json")))
+            {
+                outputFile.WriteLine(jsonString);
+            }
+        }
 
-            Resources.Add("taskList", taskTree);
+        void Open()
+        {
+            try
+            {
+                string currentPath = Environment.CurrentDirectory;
+                string jsonString;
+                using (StreamReader sr = new StreamReader(Path.Combine(currentPath, "appStore.json")))
+                {
+                    jsonString = sr.ReadToEnd();
+                }
+                AppStore appStore = JsonSerializer.Deserialize<AppStore>(jsonString)!;
+
+                TaskItem root = appStore.taskTree.Items[0];
+                appStore.taskTree.SetParentOnChildTasks(root);
+                Current.Resources.Add("taskList", appStore.taskTree);
+
+                Timer.Duration = appStore.settings.Duration;
+                Timer.autoPausePlay = appStore.settings.autoPausePlayEnabled;
+                Timer.autoFocus = appStore.settings.autoFocusEnabled;
+                Timer.autoPauseKey = appStore.settings.autoPauseKey;
+                Timer.SetReinforcement(appStore.settings.reinforcement);
+                Timer.SetAlarmName(appStore.settings.alarmName);
+                try
+                {
+                    Timer.autoFocusClickLocation = new Location(x: appStore.settings.autoFocusClickLocation!.x, y: appStore.settings.autoFocusClickLocation.y);
+                    Timer.autoPlayClickLocation = new Location(x: appStore.settings.autoPlayClickLocation!.x, y: appStore.settings.autoPlayClickLocation.y);
+                } catch { 
+                }
+            }
+            catch
+            {
+                TaskItem parent = new TaskItem();
+                parent.SetTask("Your Objective");
+                TaskTree taskTree = new TaskTree(parent);
+                taskTree.SetCurrentTask(parent);
+                Current.Resources.Add("taskList", taskTree);
+            }
+        }
+
+        void Application_Exit(object sender, ExitEventArgs e)
+        {
+            this.Save();
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            this.Open();
         }
     }
 }
